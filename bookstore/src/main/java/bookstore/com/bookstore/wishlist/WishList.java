67c498f741c868b5defb102f2ebf608747af7646
geral <geraldc29@gmail.com>
2022-04-15 19:31:42 -0400

    commited wishlist file

---
+++ bookstore/src/main/java/bookstore/com/bookstore/wishlist/WishList.java
@@ -0,0 +1,83 @@
+package bookstore.com.bookstore.wishlist;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "wishlist")
+@Table
+public class WishList {
+
+    @Id
+    @Column(
+            name = "wishlistid"
+    )
+    private Integer wishlistId;
+
+    @Column(
+            name = "accountid"
+    )
+    private Integer accountId;
+
+    @Column(
+            name = "wishlistname"
+    )
+    private String wishlistName;
+
+    @Column(
+            name = "books"
+    )
+    private String Books;
+
+    public WishList(){
+
+    }
+
+    public WishList(int wishlistId, String wishlistName, int accountId, String Books) {
+        this.wishlistId = wishlistId;
+        this.wishlistName = wishlistName;
+        this.accountId = accountId;
+        this.Books = Books;
+    }
+
+    public Integer getwishlistId() {
+        return wishlistId;
+    }
+
+    public void setwishlistId(Integer wishlistId) {
+        this.wishlistId = wishlistId;
+    }
+
+    public String getwishlistName() {
+        return wishlistName;
+    }
+
+    public void setwishlistName(String wishlistName) {
+        this.wishlistName = wishlistName;
+    }
+
+    public Integer getaccountId() {
+        return accountId;
+    }
+
+    public void setgetaccountId(Integer accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getBooks() {
+        return Books;
+    }
+
+    public void setBooks(String Books){this.Books = Books;}
+
+    @Override
+    public String toString() {
+        return "Wishlist{" +
+                "wishlistId=" + wishlistId +
+                ", wishlistName='" + wishlistName + "accountId " + accountId + "Books" + Books + '\'' +
+                '}';
+    }
+}
+//Must have 3 different wish lists
+// might have to change wishlistid and name to bookid and name since they are both referring to the same thing
+//techicanlly speaking
---
+++ bookstore/src/main/java/bookstore/com/bookstore/wishlist/WishListRepository.java
@@ -0,0 +1,5 @@
+package bookstore.com.bookstore.wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WishListRepository extends JpaRepository<WishList,Integer>{
+}

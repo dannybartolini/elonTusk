4f69004ad708628f19f25054aea0d3d572b4a00d
geral <geraldc29@gmail.com>
2022-04-16 14:47:12 -0400

    please work

--- bookstore/src/main/java/bookstore/com/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/bookstore/com/bookstore/BookstoreApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 @RestController
@@ -30,6 +31,8 @@ public class BookstoreApplication /*implements CommandLineRunner */{
 	 public String helloWorld(){
 	 	return "Hello there, welcome to Elon Musks's bookstore";
 	 }
+
+
 
 //	 @Bean
 //	 CommandLineRunner commandLineRunner(AccountRepository accountRepository) {
